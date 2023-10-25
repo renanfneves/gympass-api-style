06b6682719c491886d2f013e36e9721c13b2fa4b
Renan Floriano Neves <renanfneves@gmail.com>
2023-10-25 14:06:10 -0300

    feat: create first use case applying layered clean architecture concepts

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,31 @@
 import fastify from 'fastify'
+import { appRoutes } from './http/routes'
+import { ZodError } from 'zod'
+import { env } from './env'
 
 export const app = fastify()
 
 app.get('/ping', (request, reply) => {
   reply.send({ pong: '🏓' })
+})
+
+app.register(appRoutes)
+
+app.setErrorHandler((error, _, reply) => {
+  if (error instanceof ZodError) {
+    return reply.status(400).send({
+      message: 'Validation error.',
+      issues: error.format(),
+    })
+  }
+
+  if (env.NODE_ENV !== 'production') {
+    console.error(error)
+  } else {
+    // TODO: add log tool
+  }
+
+  return reply.status(500).send({
+    message: 'Internal server error.',
+  })
 })
