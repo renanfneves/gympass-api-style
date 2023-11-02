1855abc9d6ca06e079678d1c7c17a9a9d4dcc97b
Renan Floriano Neves <renanfneves@gmail.com>
2023-11-02 20:32:14 -0300

    chore: setup prisma test environment to generate random database schemas for each test switch

--- prisma/vitest-environment-prisma/prisma-test-environment.ts
+++ prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -1,16 +1,48 @@
-import { Environment } from 'vitest'
+import 'dotenv/config'
 
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
+import { randomUUID } from 'node:crypto'
+import { execSync } from 'node:child_process'
+import { Environment } from 'vitest'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+function generateDatabaseURL(schema: string) {
+  if (!process.env.DATABASE_URL) {
+    throw new Error('Please provide a DATABASE_URL environment variable.')
+  }
+
+  // since database url is a real url we need to manipulate this var to generate a new url with random database schema
+  // each test switch will have its own database schema
+  const url = new URL(process.env.DATABASE_URL)
+
+  url.searchParams.set('schema', schema)
+
+  return url.toString()
+}
+
 export default <Environment>{
   name: 'prisma',
   transformMode: 'web',
   async setup() {
-    console.log('Setup')
+    const schema = randomUUID()
+    const databaseURL = generateDatabaseURL(schema)
+
+    // change DATABASE_URL env var to the new one with random schema
+    process.env.DATABASE_URL = databaseURL
+
+    // executing migrations using node execSync with deploy - migrate deploy skips the db schema comparing
+    execSync('npx prisma migrate deploy')
 
     return {
       async teardown() {
-        console.log('Teardown')
+        // finally excluding the new database schema created for the test switch
+        await prisma.$executeRawUnsafe(
+          `DROP SCHEMA IF EXISTS "${schema}" CASCADE`,
+        )
+
+        // and disconnect to database
+        await prisma.$disconnect()
       },
     }
   },
