401221e52cab3575323eb4c67e7a4cc8f44c5ce2
Renan Floriano Neves <renanfneves@gmail.com>
2023-11-02 22:11:19 -0300

    feat: implement all missing controllers

---
+++ src/http/controllers/users/register.spec.ts
@@ -0,0 +1,25 @@
+import request from 'supertest'
+import { app } from '@/app'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+
+describe('Register (e2e)', () => {
+  // for test e2e with supertest we must wait till app be ready
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  // for test e2e with supertest we must wait till app be closed
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to register', async () => {
+    const response = await request(app.server).post('/users').send({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123456',
+    })
+
+    expect(response.statusCode).toEqual(201)
+  })
+})
